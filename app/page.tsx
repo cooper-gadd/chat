214001f698e2f1a4b201a8117f6fc092f0a0da31
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2025-11-14 17:18:48 -0500

    actually centered

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import { Suggestions } from "@/components/suggestions";
 
 export default function Page() {
   return (
-    <div className="flex flex-col min-h-screen gap-4 justify-center p-10 w-full">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">
-        How can I help today?
-      </h1>
-      <Suggestions />
-    </div>
+    <main className="flex min-h-screen items-center justify-center p-6 md:p-10 w-full">
+      <div className="gap-4 flex flex-col">
+        <h1 className="scroll-m-20 text-balance text-4xl font-extrabold tracking-tight">
+          How can I help today?
+        </h1>
+        <Suggestions />
+      </div>
+    </main>
   );
 }
