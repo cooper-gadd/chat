4af07d5898affd7f7bea1c3dff2c2bf1fc730b2e
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2025-11-17 11:33:58 -0500

    postgres not bun :(

--- db/index.ts
+++ db/index.ts
@@ -1,25 +1,17 @@
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/neon-http";
-import { SQL } from "bun";
-import * as schema from "./schema";
-import { neon } from "@neondatabase/serverless";
-import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
-config({ path: ".env" });
+import * as schema from "./schema";
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  conn: SQL | undefined;
+  conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? new SQL(process.env.DATABASE_URL!);
+const conn = globalForDb.conn ?? postgres(process.env.DATABASE_URL!);
+if (process.env.NODE_ENV !== "production") globalForDb.conn = conn;
 
-if (process.env.NODE_ENV !== "production") {
-  globalForDb.conn = conn;
-}
-
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql, schema });
+export const db = drizzle(conn, { schema });
