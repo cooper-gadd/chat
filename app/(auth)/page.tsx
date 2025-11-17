a70ce00d9d5858e47a2191efe509eaa2e9e303bb
Cooper Gadd <126418690+cooper-gadd@users.noreply.github.com>
2025-11-17 00:12:49 -0500

    rm empty

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -7,13 +7,18 @@ import {
   InputGroupTextarea,
 } from "@/components/ui/input-group";
 import { ArrowUpIcon } from "lucide-react";
-import { Empty } from "@/components/empty";
 import { createThread } from "@/actions/create-thread";
 import { usePendingMessage } from "@/context/pending-message";
 import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { suggestions } from "@/data/suggestions";
 
 export default function Page() {
   const [input, setInput] = useState("");
+  const [selected, setSelected] = useState<string>(suggestions[0].title);
+  const suggestionOptions = suggestions.find(
+    (suggestion) => selected === suggestion.title,
+  )?.options;
   const { setPendingMessage } = usePendingMessage();
 
   async function send() {
@@ -28,7 +33,42 @@ export default function Page() {
 
   return (
     <main className="flex h-full justify-center items-center p-3 md:p-5 w-full flex-col">
-      <Empty setInput={setInput} />
+      <div className="flex-1 flex items-center justify-center w-full">
+        <div className="gap-4 flex flex-col items-start max-w-3xl">
+          <h1 className="scroll-m-20 text-balance text-4xl font-extrabold tracking-tight text-center">
+            How can I help today?
+          </h1>
+          <div className="flex gap-2">
+            {suggestions.map((suggestion) => (
+              <Button
+                className="rounded-full"
+                variant={selected === suggestion.title ? "default" : "outline"}
+                onClick={() => setSelected(suggestion.title)}
+                key={suggestion.title}
+              >
+                <suggestion.icon />
+                {suggestion.title}
+              </Button>
+            ))}
+          </div>
+          <div className="text-foreground flex flex-col w-full">
+            {suggestionOptions!.map((option) => (
+              <div
+                key={option}
+                className="border-secondary flex items-start gap-2 border-t py-1 first:border-none"
+              >
+                <button
+                  onClick={() => setInput(option)}
+                  className="text-secondary-foreground hover:bg-secondary w-full rounded-md p-2 text-left"
+                >
+                  {option}
+                </button>
+                <div />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
       <form className="w-full max-w-3xl" onSubmit={() => send()}>
         <InputGroup className="max-w-3xl">
           <InputGroupTextarea
